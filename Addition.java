8e701c04e6fa14799099ead078dcd957c29ce8e8
TejasJagdale0909 <tejasjjagdale999@gmail.com>
2025-09-20 11:27:35 +0530

    Update Addition.java

--- Addition.java
+++ Addition.java
@@ -2,9 +2,10 @@ class Addition {
     public static void main (String [] args) {
         int a = 10;
         int b = 20;
-        int c = 30;
+        int c = 300;
 
         int Addition = (a+b+c);
         System.out.println("Addition of 3 numbers is : "+Addition);
     }
+
 }
